c61df32b6bf5b9617b1cedb89cd4f186888b8963
RD <81473475+nylesor28@users.noreply.github.com>
2021-07-17 17:50:58 -0400

    add logic to dynamically build the daily five day forecast section

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,10 @@ var apikey="deb384ba8b93ecfc669990c7064f55ad"
 
 var historyArray=[]
 
+var formatDate = function(date){
+    return date.toLocaleDateString('en-US')
+}
+
 var setLocalStorage = function (key,value) {
     localStorage.setItem(key, JSON.stringify(value));
 };
@@ -27,8 +31,8 @@ var getLocalStorage= function (key) {
     return (JSON.parse(localStorage.getItem(key)));
 };
 
-var getUVIndex= function(lat,lon){
-    var exclusion = "minutely,hourly,daily,alerts"
+var getAllForecast= function(lat,lon){
+    var exclusion = "minutely,hourly,alerts"
     var units = "imperial"
     var apiURL = `${openWeatherAPIURL}lat=${lat}&lon=${lon}&exclude=${exclusion}&appid=${apikey}&units=${units}`
     console.log(apiURL)
@@ -39,6 +43,7 @@ var getUVIndex= function(lat,lon){
  
 }
 
+
 var getCityForecast= function(city){
     var weatherAPIURL =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=imperial`;
     console.log(weatherAPIURL)
@@ -47,19 +52,60 @@ var getCityForecast= function(city){
     return response.json();
     })
 }
-
 var getWeather = async function(city){
+    const iconURL = " http://openweathermap.org/img/w/"
 
+    //get forecast for for searched city
     var currentData =await getCityForecast(city);
     console.log("currentData: ", currentData)
+
+    //display current weather information
     searchCityEl.textContent=currentData.name;
+    var currentDate= `(${formatDate(new Date(currentData.dt*1000))})`
+    todayDateEl.textContent=currentDate
     tempEl.textContent=currentData.main.temp;
     windEl.textContent=currentData.wind.speed
     humidityEl.textContent=currentData.main.humidity;
 
-    var uvData=await getUVIndex(currentData.coord.lat,currentData.coord.lon)
-    console.log(uvData, uvData.lat)
-     uvIndexEl.textContent =( await getUVIndex(currentData.coord.lat,currentData.coord.lon)).current.uvi;
+    var iconImgUrl = `${iconURL}${currentData.weather[0].icon}.png`
+    console.log("imageIcon", iconImgUrl)
+
+    weatherIconEl.textContent=""
+    var img = document.createElement("img");
+    img.setAttribute("src", iconImgUrl)
+    img.setAttribute("alt", currentData.weather[0].description)
+    weatherIconEl.appendChild(img)
+
+    var allForecastData=await getAllForecast(currentData.coord.lat,currentData.coord.lon)
+    console.log(allForecastData, allForecastData.lat)
+    uvIndexEl.textContent =(await getAllForecast(currentData.coord.lat,currentData.coord.lon)).current.uvi;
+
+    fiveDayContainerEl.textContent =""
+    for(var i=1; i<6; i++){
+        var dailyInfo = allForecastData.daily[i]
+        var daytime = `${formatDate(new Date(dailyInfo.dt*1000))}`
+        var dayTemp = dailyInfo.temp.day;
+        var dayWind = dailyInfo.wind_speed
+        var dayHumidity = dailyInfo.humidity;
+        var dayIconLocation = `${iconURL}${dailyInfo.weather[0].icon}.png`;
+        var dayIconDescription = dailyInfo.weather[0].description;
+
+        // var fiveDay = 
+        fiveDayContainerEl.innerHTML+=
+        `
+            <div class="five-day col-md-2">
+                <p class="forecast-date">${daytime}</p>
+                <p class="forecast-icon"><img src="${dayIconLocation}" alt="${dayIconDescription}"></p>
+                <p>Temp: <span class="forecast-temp">${dayTemp}</span></p>
+                <p>Wind: <span class="forecast-wind">${dayWind}</span></p>
+                <p>Humidity: <span class="forecast-humidity">${dayHumidity}</span></p>
+            </div>
+        `
+        // fiveDayContainerEl.append(fiveDay)
+        console.log(i, daytime, dayTemp, dayWind, dayHumidity, dayIconLocation, dayIconDescription)
+    }
+
+
 
 }
 
